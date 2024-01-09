bc232720a872f45fe0ff010917b47020c41c8cb8
Enzo Luigi Schork <enzoluigischork@gmail.com>
2024-01-09 09:24:12 -0300

    0.2

---
+++ Socialcademy/View Models/PostViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  PostViewModel.swift
+//  Socialcademy
+//
+//  Created by Enzo Schork on 09/01/24.
+//
+
+import Foundation
+
+@MainActor
+class PostsViewModel: ObservableObject {
+    @Published var posts = [Post.testPost]
+    
+    func makeCreateAction() -> NewPostForm.CreateAction {
+        return { [weak self] post in
+            try await PostsRepository.create(post)
+            self?.posts.insert(post, at: 0)
+        }
+    }
+}
