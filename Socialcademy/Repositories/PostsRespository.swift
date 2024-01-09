7a20d271b7660cbf2e913cd43a65e5b239c55377
Enzo Luigi Schork <enzoluigischork@gmail.com>
2024-01-09 11:21:43 -0300

    0.3

--- Socialcademy/Repositories/PostsRespository.swift
+++ Socialcademy/Repositories/PostsRespository.swift
@@ -9,10 +9,40 @@ import Foundation
 import FirebaseFirestore
 import FirebaseFirestoreSwift
 
-struct PostsRepository {
-    static let postsReference = Firestore.firestore().collection("posts")
+// MARK: - PostsRepositoryProtocol
+protocol PostsRepositoryProtocol {
+    func fetchPosts() async throws -> [Post]
+    func create(_ post: Post) async throws
+}
+
+// MARK: - PostsRepositoryStub
+
+#if DEBUG
+struct PostsRepositoryStub: PostsRepositoryProtocol {
+    let state: Loadable<[Post]>
     
-    static func create(_ post: Post) async throws {
+    func fetchPosts() async throws -> [Post] {
+        return try await state.simulate()
+    }
+    
+    func create(_ post: Post) async throws {}
+}
+#endif
+
+// MARK: - PostsRepository
+struct PostsRepository: PostsRepositoryProtocol {
+    let postsReference = Firestore.firestore().collection("posts")
+    
+    func fetchPosts() async throws -> [Post] {
+        let snapshot = try await postsReference
+            .order(by: "timestamp", descending: true)
+            .getDocuments()
+        return snapshot.documents.compactMap { document in
+            try! document.data(as: Post.self)
+        }
+    }
+    
+    func create(_ post: Post) async throws {
         let document = postsReference.document(post.id.uuidString)
         try await document.setData(from: post)
     }
@@ -21,8 +51,6 @@ struct PostsRepository {
 private extension DocumentReference {
     func setData<T: Encodable>(from value: T) async throws {
         return try await withCheckedThrowingContinuation { continuation in
-            // Method only throws if there’s an encoding error, which indicates a problem with our model.
-            // We handled this with a force try, while all other errors are passed to the completion handler.
             try! setData(from: value) { error in
                 if let error = error {
                     continuation.resume(throwing: error)
--- Socialcademy/Views/PostList.swift
+++ Socialcademy/Views/PostList.swift
@@ -5,6 +5,7 @@
 //  Created by Enzo Schork on 09/01/24.
 //
 
+import Foundation
 import SwiftUI
 
 struct PostsList: View {
@@ -15,12 +16,32 @@ struct PostsList: View {
     
     var body: some View {
         NavigationView {
-            List(viewModel.posts) { post in
-                if searchText.isEmpty || post.contains(searchText) {
-                    PostRow(post: post)
+            Group {
+                switch viewModel.posts {
+                case .loading:
+                    ProgressView()
+                case let .error(error):
+                    EmptyListView(
+                        title: "Cannot Load Posts",
+                        message: error.localizedDescription,
+                        retryAction: {
+                            viewModel.fetchPosts()
+                        }
+                    )
+                case .empty:
+                    EmptyListView(
+                        title: "No Posts",
+                        message: "There aren’t any posts yet."
+                    )
+                case let .loaded(posts):
+                    List(posts) { post in
+                        if searchText.isEmpty || post.contains(searchText) {
+                            PostRow(post: post)
+                        }
+                    }
+                    .searchable(text: $searchText)
                 }
             }
-            .searchable(text: $searchText)
             .navigationTitle("Posts")
             .toolbar {
                 Button {
@@ -33,11 +54,30 @@ struct PostsList: View {
                 NewPostForm(createAction: viewModel.makeCreateAction())
             }
         }
+        .onAppear {
+            viewModel.fetchPosts()
+        }
     }
 }
 
+#if DEBUG
 struct PostsList_Previews: PreviewProvider {
     static var previews: some View {
-        PostsList()
+        ListPreview(state: .loaded([Post.testPost]))
+        ListPreview(state: .empty)
+        ListPreview(state: .error)
+        ListPreview(state: .loading)
+    }
+    
+    @MainActor
+    private struct ListPreview: View {
+        let state: Loadable<[Post]>
+        
+        var body: some View {
+            let postsRepository = PostsRepositoryStub(state: state)
+            let viewModel = PostsViewModel(postsRepository: postsRepository)
+            PostsList(viewModel: viewModel)
+        }
     }
 }
+#endif
