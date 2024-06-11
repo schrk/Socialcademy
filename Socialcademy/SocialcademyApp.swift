7c1240ac3b99937631b91011de9b921b2ba8c9be
Enzo Luigi Schork <enzoluigischork@gmail.com>
2024-06-11 11:03:13 -0300

    Initial Commit

---
+++ Socialcademy/SocialcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialcademyApp.swift
+//  Socialcademy
+//
+//  Created by Havan on 11/06/24.
+//
+
+import SwiftUI
+
+@main
+struct SocialcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
