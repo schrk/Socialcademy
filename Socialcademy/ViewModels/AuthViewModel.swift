0e3c6997aa23d7fc0d70b0d4257474d7a29da74d
Enzo Luigi Schork <enzoluigischork@gmail.com>
2024-06-13 11:53:05 -0300

    feat: firebase-auth
    Para realizar a criação do seu pull request, especifique o objetivo do mesmo:
    - [X] New Feature
    - [ ] Refactor
    - [ ] Bug
    
    Nesta pull request, fui encarregado de:
    ## O quê?
    Autenticacao para o usuario
    
    ## Como?
    Adicionando e utilizando a biblioteca do firebase auth
    
    ## Por quê?
    Para que usuários respectivos tenham acesso somente ao seus posts

---
+++ Socialcademy/ViewModels/AuthViewModel.swift
@@ -0,0 +1,53 @@
+//
+//  AuthViewModel.swift
+//  Socialcademy
+//
+//  Created by schork on 13/06/24.
+//
+
+import Foundation
+
+@MainActor
+class AuthViewModel: ObservableObject {
+    @Published var email = ""
+    @Published var password = ""
+    @Published var isAuthenticated = false
+    
+    private let authService = AuthService()
+    
+    init() {
+        authService.$isAuthenticated.assign(to: &$isAuthenticated)
+    }
+    
+    func signIn() {
+        Task {
+            do {
+                try await authService.signIn(email: email, password: password)
+            } catch {
+                print("[AuthViewModel] Cannot sign in: \(error)")
+            }
+        }
+    }
+    
+    func makeSignInViewModel() -> SignInViewModel {
+        return SignInViewModel(action: authService.signIn(email:password:))
+    }
+
+    func makeCreateAccountViewModel() -> CreateAccountViewModel {
+        return CreateAccountViewModel(action: authService.createAccount(name:email:password:))
+    }
+}
+
+extension AuthViewModel {
+    class SignInViewModel: FormViewModel<(email: String, password: String)> {
+        convenience init(action: @escaping Action) {
+            self.init(initialValue: (email: "", password: ""), action: action)
+        }
+    }
+    
+    class CreateAccountViewModel: FormViewModel<(name: String, email: String, password: String)> {
+        convenience init(action: @escaping Action) {
+            self.init(initialValue: (name: "", email: "", password: ""), action: action)
+        }
+    }
+}
